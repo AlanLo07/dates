cb39a3ee43ba17f885f204bcb8d7dae656a3985d
Alan Lopez <alan.lopezg9707@gmail.com>
2025-12-16 12:28:10 -0600

    nuevo index

--- tool/generate_index.dart
+++ tool/generate_index.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 void main(List<String> args) {
   final outPath = args.isNotEmpty ? args[0] : 'web/custom_index.html';
   final title = args.length > 1 ? args[1] : 'Página estática generada';
-  final content = '''<!doctype html>
+  final content =
+      '''<!doctype html>
 <html lang="es">
   <head>
     <meta charset="utf-8" />
