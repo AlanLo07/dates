c384e161b43cc8f6cf833115a844f6ce58d6f0f0
Alan Lopez <alan.lopezg9707@gmail.com>
2026-06-12 16:23:47 -0600

    Refactor code structure for improved readability and maintainability

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4019393870"
+    serviceWorkerVersion: "2924347904"
   }
 });
