2bbc22f2ef455a830052d898b95aa9888578c5d7
Alan Lopez <alanricardo.lopez.contractor@bbva.com>
2026-07-07 14:16:25 -0600

    refactor: Simplify FriendlyActionButton by removing unnecessary animation logic

--- lib/screens/phrases/widgets/friendly_action_button.dart
+++ lib/screens/phrases/widgets/friendly_action_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../../utils/animations.dart';
 import '../../../widgets/motion/motion_pressable.dart';
 
 class FriendlyActionButton extends StatefulWidget {
@@ -24,52 +23,41 @@ class FriendlyActionButton extends StatefulWidget {
 }
 
 class _FriendlyActionButtonState extends State<FriendlyActionButton> {
-  bool _isPressed = false;
-
   @override
   Widget build(BuildContext context) {
     return MotionPressable(
       onTap: widget.onPressed,
       pressedScale: 0.96,
       borderRadius: BorderRadius.circular(30),
-      child: AnimatedScale(
-        duration: MotionDurations.micro,
-        scale: _isPressed ? 0.98 : 1,
-        child: GestureDetector(
-          onTapDown: (_) => setState(() => _isPressed = true),
-          onTapCancel: () => setState(() => _isPressed = false),
-          onTapUp: (_) => setState(() => _isPressed = false),
-          child: Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(vertical: 12),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color.lerp(widget.backgroundColor, Colors.white, 0.10) ??
-                      widget.backgroundColor,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.lerp(widget.backgroundColor, Colors.white, 0.10) ??
                   widget.backgroundColor,
-                ],
-              ),
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(widget.icon, size: 20, color: widget.foregroundColor),
-                const SizedBox(width: 8),
-                Text(
-                  widget.label,
-                  style: TextStyle(
-                    color: widget.foregroundColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
-                  ),
-                ),
-              ],
-            ),
+              widget.backgroundColor,
+            ],
           ),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(widget.icon, size: 20, color: widget.foregroundColor),
+            const SizedBox(width: 8),
+            Text(
+              widget.label,
+              style: TextStyle(
+                color: widget.foregroundColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+              ),
+            ),
+          ],
         ),
       ),
     );
